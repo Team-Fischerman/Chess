0bd83f620ecba8ab6e19cae4621928e760e67fda
Harvey Munoz <zyhrups3@gmail.com>
2021-11-03 23:31:31 -0400

    first commit

---
+++ Checkmate/Chess.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Checkmate
+{
+    public partial class Chess : Form
+    {
+        public Chess()
+        {
+            InitializeComponent();
+        }
+    }
+}
